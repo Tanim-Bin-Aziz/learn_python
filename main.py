9458a34340d72101c913104d882828418df4b317
Tanim Bin Aziz <tanimbinaziz145@gmail.com>
2026-04-19 19:09:37 +0600

    th class 1

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-name = "Tanim"
-print("Hello,", name)
+# name = "Tanim"
+# print("Hello,", name)
+
+
+''' 
+Note for python
+
+interger = int 123 -123 ☑️ 1.2 - 2.5 ❌
+character = char a Z $ ☑️
+string = str "Hello World" 'Hello World' ☑️
+boolean = bool True False ☑️
+
+'''
+a = 10
+b = 20 
+sum = a + b
+print("The sum of a + b =", sum);
