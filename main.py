e6eaa0dbcf4b06b556a84e85e84f349111cce0e6
Tanim Bin Aziz <tanimbinaziz145@gmail.com>
2026-04-19 20:36:26 +0600

    calculations done

--- main.py
+++ main.py
@@ -11,7 +11,26 @@ string = str "Hello World" 'Hello World' ☑️
 boolean = bool True False ☑️
 
 '''
-a = 10
-b = 20 
-sum = a + b
-print("The sum of a + b =", sum);
+# a = 10
+# b = 20 
+# sum = a + b
+# print("The sum of a + b =", sum);
+
+
+
+
+# a = int(input("enter value"));
+# print (a)
+# print (type(a)) # by default input is string
+
+# b = "somthings" + a;
+# print (b) # error because we can't add string and int
+
+
+a = float(input("enter value"));
+b = float(input("enter value"));
+
+print(f"sum = {a + b}")
+print(f"sub = {a - b}")
+print(f"mul = {a * b}")
+print(f"div = {a / b}")
